49b8a80838a9aa68b9e7da15faf536cdf2ad242b
Pablo Balduz <9513953+pbalduz@users.noreply.github.com>
2022-11-01 12:19:49 +0100

    Add test for property wrapper's reset method

--- Tests/xctest-resettableTests/ResettableTestCaseTests.swift
+++ Tests/xctest-resettableTests/ResettableTestCaseTests.swift
@@ -5,15 +5,41 @@ class ResettableTestCaseTests: XCTestCase {
     
     func testResettableValueIsSetToNilOnTearDown() {
         let sut = TestCaseStub()
-        XCTAssertEqual(sut.resettableValue, "resettable")
-        XCTAssertEqual(sut.nonResettableValue, "non resettable")
+        XCTAssertEqual(sut.resettableString, "resettable")
+        XCTAssertEqual(sut.nonResettableString, "non resettable")
         sut.tearDown()
-        XCTAssertNil(sut.resettableValue)
-        XCTAssertEqual(sut.nonResettableValue, "non resettable")
+        XCTAssertNil(sut.resettableString)
+        XCTAssertEqual(sut.nonResettableString, "non resettable")
+    }
+    
+    func testResettableResetMethodInvokedOnTearDown() {
+        let sut = TestCaseStub()
+        XCTAssertFalse(sut.resetWasCalled)
+        sut.setUp()
+        sut.tearDown()
+        XCTAssertTrue(sut.resetWasCalled)
     }
 }
 
 private class TestCaseStub: ResettableTestCase {
-    @Resettable var resettableValue = "resettable"
-    var nonResettableValue = "non resettable"
+    @Resettable var resettableString = "resettable"
+    var nonResettableString = "non resettable"
+    
+    struct TestValue {
+        let resetClosure: () -> Void
+        
+        func reset() {
+            resetClosure()
+        }
+    }
+    
+    @Resettable(onReset: TestValue.reset)
+    var resettableValue: TestValue!
+    
+    var resetWasCalled = false
+    
+    override func setUp() {
+        super.setUp()
+        self.resettableValue = TestValue(resetClosure: { self.resetWasCalled = true })
+    }
 }
