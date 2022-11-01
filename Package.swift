56cfc126d66f8de1bede0ecb5e8aa0068affea52
Pablo Balduz <9513953+pbalduz@users.noreply.github.com>
2022-11-01 18:48:04 +0100

    Add platforms to package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,10 @@ import PackageDescription
 
 let package = Package(
     name: "xctest-resettable",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v10_15)
+    ],
     products: [
         .library(name: "TestResettable", targets: ["TestResettable"]),
     ],
